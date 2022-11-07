e1f134b6f3bd8aa17c74893cd28adeb8ac9c8030
andrew <andrew>
2022-11-07 19:08:45 +0000

    Edited files

--- python-runnables/create-ssh-key-and-add-git-config/runnable.py
+++ python-runnables/create-ssh-key-and-add-git-config/runnable.py
@@ -54,7 +54,7 @@ class MyRunnable(Runnable):
         else:
             print('Generating SSH Key')
             try:
-                ssh_key = generate_key(self.project_key)
+                ssh_key, stderr = generate_key(self.project_key)
                 print('Generated SSH Key: {}'.format(ssh_key))
             except:
                 raise Exception('Failed to generate ssh key.')
